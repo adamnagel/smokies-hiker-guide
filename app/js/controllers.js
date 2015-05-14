7c20854bd93347f91f6f7dd43a1c177360fd90c8
Adam Nagel <adam.nagel+git@gmail.com>
2015-05-14 05:12:39 -0500

    Add some more controls and data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,5 +7,25 @@ var smokiesHikerGuideApp = angular.module('smokiesHikerGuideApp', ['ngMaterial']
 smokiesHikerGuideApp.controller('HikerGuideCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('hikes/hikes.json').success(function(data) {
     $scope.hikes = data;
+    $scope.orderProp = 'length';
+    $scope.featureFilter = {};
+
+    /*  This filter function reads the dictionary of feature filters.
+        For those that have 'true', it checks if a hike has that feature.
+        If it doesn't, then it fails.
+        If none fail, then it succeeds.
+     */
+    $scope.filterByFeature = function(hike) {
+      for (var feature in $scope.featureFilter) {
+        if ($scope.featureFilter.hasOwnProperty(feature)) {
+          if ($scope.featureFilter[feature]) {
+            if (hike.goodfor.indexOf(feature) < 0) {
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    }
   });
 }]);
