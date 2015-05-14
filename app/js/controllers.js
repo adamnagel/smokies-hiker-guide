30eb79715dca7634c1f7f27726ec56f22f912d45
Adam Nagel <adam.nagel+git@gmail.com>
2015-05-14 04:01:06 -0500

    Messing around. Not really working.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,10 @@
 
 /* Controllers */
 
-var smokiesHikerGuideApp = angular.module('smokiesHikerGuideApp', []);
+var smokiesHikerGuideApp = angular.module('smokiesHikerGuideApp', ['ngMaterial']);
 
 smokiesHikerGuideApp.controller('HikerGuideCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('hikes/hikes.json').success(function(data) {
     $scope.hikes = data;
   });
-
-
 }]);
